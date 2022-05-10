e41d56e777fd57c04acba7e9b484dd58f0ca070f
twilikiss <mlp52013140@163.com>
2022-05-10 11:34:41 +0800

    v6.6.6

--- src/main/java/test01/java_project.java
+++ src/main/java/test01/java_project.java
@@ -4,7 +4,7 @@ public class java_project {
     public static void main(String[] args) {
         System.out.println("hello Git1!");
         System.out.println("hello Git2!");
-        System.out.println("hello Git3!");
+        System.out.println("hello Git3! 这行好啊！");
         System.out.println("hello Git4!");
         System.out.println("hello Git5!");
         System.out.println("hello Git6!");
